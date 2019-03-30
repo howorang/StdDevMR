bf3dd6e16ba7a41fac910728f376d89fa23b0aec
Piotr Borczyk <howorang@gmail.com>
2019-03-30 23:54:59 +0100

    works

--- src/main/java/dmcs/mapreduce/StdDev.java
+++ src/main/java/dmcs/mapreduce/StdDev.java
@@ -15,89 +15,73 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StdDev {
 
-    public static class Key {
-        private final String side;
-        private final int serie;
-        private final String finger;
+        public static class JsonMapper extends Mapper<Object, Text, Text, DoubleWritable> {
 
-        public Key(String side, int serie, String finger) {
-            this.side = side;
-            this.serie = serie;
-            this.finger = finger;
-        }
+            private static JsonParser parser = new JsonParser();
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Key key = (Key) o;
-            return serie == key.serie &&
-                    Objects.equals(side, key.side) &&
-                    Objects.equals(finger, key.finger);
-        }
-
-        @Override
-        public int hashCode() {
-
-            return Objects.hash(side, serie, finger);
-        }
-    }
-
-    public static class JsonMapper extends Mapper<Object, Text, Key, DoubleWritable> {
-
-        private static JsonParser parser = new JsonParser();
-
-        @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            JsonObject json = parser.parse(value.toString()).getAsJsonObject();
-            String side = json.get("side").getAsString();
-            int series = json.get("series").getAsInt();
-            for (Map.Entry<String, JsonElement> field : json.get("features2D").getAsJsonObject().entrySet()) {
-                String finger = field.getKey();
-                double featureValue = field.getValue().getAsDouble();
-                context.write(new Key(side, series, finger), new DoubleWritable(featureValue));
+            @Override
+            public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+                JsonObject json = parser.parse(value.toString()).getAsJsonObject();
+                String side = json.get("side").getAsString();
+                int series = json.get("series").getAsInt();
+                for (Map.Entry<String, JsonElement> field : json.get("features2D").getAsJsonObject().entrySet()) {
+                    String finger = field.getKey();
+                    double featureValue = field.getValue().getAsDouble();
+                    context.write(new Text(side + "-" + series + "-" + finger), new DoubleWritable(featureValue));
+                }
             }
         }
-    }
 
-    public static class StdDevReducer extends Reducer<Key, DoubleWritable, Text, DoubleWritable> {
+        public static class StdDevReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
-        @Override
-        public void reduce(Key key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-            double avg = 0;
-            int count = 0 ;
-            for (DoubleWritable val : values) {
-                avg += val.get();
-                count++;
+            @Override
+            public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+                double avg = 0;
+                int count = 0;
+                List<Double> values_ = new ArrayList<>();
+                values.forEach(p -> values_.add(p.get()));
+                for (Double val : values_) {
+                    avg += val;
+                    count++;
+                }
+                avg = avg / count;
+
+
+                double sum = 0;
+                for (Double val : values_) {
+                    sum += Math.pow((val - avg), 2);
+                }
+                double stdDev = Math.sqrt(sum / count);
+                context.write(new Text(key.toString() + " stdDev(1," + count + ")"), new DoubleWritable(stdDev));
             }
-            avg = avg/count;
 
-            double sum = 0;
-            for (DoubleWritable val : values) {
-                sum += Math.pow((val.get() - avg), 2);
-            }
-            context.write(new Text(key.side + "-" + key.serie + "-" + key.finger + " stdDev(1," + count + ")"), new DoubleWritable(Math.sqrt(sum/count)));
         }
 
-    }
+        public static void main(String[] args) throws Exception {
+            Configuration conf = new Configuration();
+            Job job = Job.getInstance(conf, "stdDev");
+            job.setJarByClass(StdDev.class);
+            job.setMapperClass(JsonMapper.class);
+            job.setReducerClass(StdDevReducer.class);
 
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "stdDev");
-        job.setJarByClass(StdDev.class);
-        job.setMapperClass(JsonMapper.class);
-        job.setCombinerClass(StdDevReducer.class);
-        job.setReducerClass(StdDevReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(DoubleWritable.class);
+
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(DoubleWritable.class);
+
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        }
 }
 
